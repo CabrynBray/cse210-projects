73cd58a860ed90ba841b907a8135ecdfddd2be5b
CabrynBray <91556711+CabrynBray@users.noreply.github.com>
2023-06-10 20:23:54 +0200

    Completed prep

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1,101);
+        int guess = -1;
+
+        while (guess != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            guess =  guess = int.Parse(userGuess);
+
+            if (guess == magicNum)
+            {
+                Console.WriteLine("YOU GUESSED IT!");
+            }
+            else if (guess > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+
+    
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        Console.Write("Enter a list of numbers, type 0 when finished.");
+        
+        List<int> NumList = new List<int>();
+
+        int number = -1;
+
+        while (number != 0)
+        {
+            
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+            
+            if (number != 0)
+            {
+                NumList.Add(number);
+            }
+        } 
+
+        int sum = 0;
+        foreach (int Num in NumList)
+        {
+            sum += Num;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = sum / NumList.Count; 
+        Console.WriteLine($"The average is: {average}");
+
+        int maxNum = NumList[0];
+        foreach (int Num in NumList)
+        {
+            if (Num > maxNum)
+            {
+                maxNum = number;
+            } 
+        }
+        Console.WriteLine($"The max is: {maxNum}");
+
+
     }
 }
