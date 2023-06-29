012614e627b43d9456fc139510910fd874443eb1
CabrynBray <91556711+CabrynBray@users.noreply.github.com>
2023-06-29 19:29:59 +0200

    completed learning activity

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+            Book book = new Book();
+
+            Console.WriteLine("Welcome to the Scripture Memorizer\n");
+            Console.WriteLine("Choose a random verse (1) or use your own (2)? ");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                book.ChooseScripture();
+            }
+            else
+            {
+                book.InputNewScripture();
+            }
+
+            Scripture scripture = new Scripture(book.GetScripture());
+
+            scripture.DisplayScripture();
+            Console.WriteLine("Press enter to hide words, or 'quit' to exit.");
+            input = Console.ReadLine();
+            while (input != "quit")
+            {
+                Console.Clear();
+                scripture.HideWords();
+                scripture.DisplayScripture();
+
+                Console.WriteLine("Press enter to hide words, or 'quit' to exit.");
+                input = Console.ReadLine();
+
+                
+        }
     }
 }
